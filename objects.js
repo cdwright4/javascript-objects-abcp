194cf04ac6ef82233acae9f6aa628992b7ce92d6
Crystal <crystal_wright@yahoo.com>
2018-01-15 02:43:20 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,7 +2,9 @@ var playlist = { artist: 'songtitles'};
 console.log(playlist)
 
 function updatePlaylist (playlist, artist, songtitle) {
-  assign(playlist,{artist: songtitle})
+  
+  playlist.assign(playlist,{artist: songtitle});
+  return playlist;
   
 }
 
